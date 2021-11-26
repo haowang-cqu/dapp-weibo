5a288b9d461be5661b0c2d9b5de07f9ba8ba477f
Hao Wang <iamwh.cn@gmail.com>
2021-11-27 00:45:18 +0800

    功能基本实现

--- app/src/index.js
+++ app/src/index.js
@@ -6,7 +6,7 @@ const App = {
   account: null,
   weibo: null,
 
-  start: async function() {
+  start: async function () {
     const { web3 } = this;
 
     try {
@@ -25,9 +25,13 @@ const App = {
     } catch (error) {
       console.error("Could not connect to contract or chain.");
     }
+
+    this.refreshHome();
+    this.refreshAdmin();
+    this.refreshMy();
   },
 
-  refreshBalance: async function() {
+  refreshBalance: async function () {
     const { getBalance } = this.meta.methods;
     const balance = await getBalance(this.account).call();
 
@@ -35,48 +39,177 @@ const App = {
     balanceElement.innerHTML = balance;
   },
 
-  createUser: async function() {
+  createUser: async function () {
     const username = $("#inputUsername").val();
-    console.log(username);
     if (username.length > 0 && username.length <= 64) {
-      await this.weibo.methods.createUser(username).send({from: this.account});
+      await this.weibo.methods.createUser(username).send({ from: this.account });
       this.weibo.events.CreateUser(function (error, event) {
-        console.log(event);
         if (!error && event.returnValues.isSuccess) {
-          alert("注册成功");
-          this.refreshMy();
-          this.refreshAdmin();
+          console.log("注册账户成功");
         }
         else if (!error && !event.returnValues.isSuccess) {
-          alert("注册失败: "+event.returnValues.message);
+          alert("账户注册失败: " + event.returnValues.message);
         }
       });
+      // 清空输入框
+      $("#inputUsername").val("");
+      this.refreshHome();
+      this.refreshMy();
+      this.refreshAdmin();
+    }
+  },
+
+  // 发微博
+  postBlog: async function () {
+    const content = $("#newBlogContent").val();
+    if (content.length > 0 && content.length <= 160) {
+      await this.weibo.methods.postBlog(content).send({ from: this.account });
+      this.weibo.events.PostBlog(function (error, event) {
+        if (!error && event.returnValues.isSuccess) {
+          console.log("微博发送成功");
+        }
+        else if (!error && !event.returnValues.isSuccess) {
+          alert("微博发送失败: " + event.returnValues.message);
+        }
+      });
+      // 清空输入框
+      $("#newBlogContent").val("");
+      this.refreshHome();
+      this.refreshMy();
+      this.refreshAdmin();
     }
   },
 
   // 刷新主页
-  refreshHome: async function() {
+  refreshHome: async function () {
+    // 获取所有blog
+    let blogList = [];
+    const userNumber = await this.weibo.methods.getUserAmount().call();
+    for (let user_id = 0; user_id < userNumber; user_id++) {
+      const userInfo = await this.weibo.methods.getUserInfo(user_id).call();
+      if (userInfo.isSuccess) {
+        const blogNumber = userInfo.blogNumber;
+        for (let blog_id = 0; blog_id < blogNumber; blog_id++) {
+          const blog = await this.weibo.methods.getBlog(user_id, blog_id).call();
+          if (blog.isSuccess) {
+            blog.user_name = userInfo.name;
+            blog.user_id = user_id;
+            blog.blog_id = blog_id;
+            blog.timestamp = parseInt(blog.timestamp) * 1000;
+            blogList.push(blog);
+          }
+        }
+      }
+    }
+    // 按时间排序
+    blogList.sort(function (a, b) {return b.timestamp - a.timestamp;});
+    let blogListDOM = "";
+    for (let i = 0; i < blogList.length; i++) {
+      const blog = blogList[i];
+      blogListDOM += `<div class="blog">
+                        <h4>${blog.user_name}</h4>
+                        <div class="blogTime">
+                          ${new Date(blog.timestamp).toLocaleString("zh-CN")} &ensp;
+                          <span user_id="${blog.user_id}" blog_id="${blog.blog_id}" onclick="App.like(this)"><i class="far fa-thumbs-up"></i></span> <span>${blog.like}</span> &ensp;
+                          <span user_id="${blog.user_id}" blog_id="${blog.blog_id}" onclick="App.dislike(this)"><i class="far fa-thumbs-down"></i></span> <span>${blog.dislike}</span>
+                        </div>
+                        <p>
+                        ${blog.content}
+                        </p>
+                      </div>`
+    }
+    $("#recentBlog").html(blogListDOM);
+  },
+
+  // 点赞
+  like: async function (likeBtn) {
+    const user_id = parseInt($(likeBtn).attr("user_id"));
+    const blog_id = parseInt($(likeBtn).attr("blog_id"));
+    await this.weibo.methods.likeBlog(user_id, blog_id).send({from: this.account});
+    this.refreshHome();
+    this.refreshMy();
+    this.refreshAdmin();
+  },
+
+  // 点踩
+  dislike: async function (dislikeBtn) {
+    const user_id = parseInt($(dislikeBtn).attr("user_id"));
+    const blog_id = parseInt($(dislikeBtn).attr("blog_id"));
+    await this.weibo.methods.dislikeBlog(user_id, blog_id).send({from: this.account});
+    this.refreshHome();
+    this.refreshMy();
+    this.refreshAdmin();
   },
 
   // 刷新个人页
-  refreshMy: async function() {
+  refreshMy: async function () {
+    const id = await this.weibo.methods.getUserId(this.account).call();
+    // 更新账户信息
+    const userInfo = await this.weibo.methods.getUserInfo(id).call();
+    if (userInfo.isSuccess) {
+      $("#myID").text(id);
+      $("#myName").text(userInfo.name);
+      $("#myAddr").text(userInfo.addr);
+      $("#myBlogNumber").text(userInfo.blogNumber);
+      $("#myLike").text(userInfo.like);
+      $("#myDislike").text(userInfo.dislike);
+      let table = "";
+      for (let blog_id = 0; blog_id < userInfo.blogNumber; blog_id++) {
+        const blog = await this.weibo.methods.getBlog(id, blog_id).call();
+        if (blog.isSuccess) {
+          // NOTE: JS处理UNIX时间戳需要*1000
+          table += `<tr>
+                    <td>${blog_id}</td>
+                    <td>${new Date(parseInt(blog.timestamp) * 1000).toLocaleString("zh-CN")}</td>
+                    <td>${blog.content}</td>
+                    <td>${blog.like}</td>
+                    <td>${blog.dislike}</td>
+                  </tr>`;
+        }
+        $("#myBlogTable").html(table);
+      }
+    }
   },
 
   // 刷新管理页
-  refreshAdmin: async function() {
-    const result = await this.weibo.methods.getAllUserAndBlogs().call();
-    $("#userAmount").text(result.userAmount);
-    $("#blogAmount").text(result.blogAmount);
+  refreshAdmin: async function () {
+    // 更新后台信息
+    const userAmount = await this.weibo.methods.getUserAmount().call();
+    const blogAmount = await this.weibo.methods.getBlogAmount().call();
+    $("#userAmount").text(userAmount);
+    $("#blogAmount").text(blogAmount);
+    // 更新用户列表
+    let table = "";
+    for (let id = 0; id < userAmount; id++) {
+      const userInfo = await this.weibo.methods.getUserInfo(id).call();
+      if (userInfo.isSuccess) {
+        table += `<tr>
+                    <td>${id}</td>
+                    <td>${userInfo.name}</td>
+                    <td>${userInfo.addr}</td>
+                    <td>${userInfo.blogNumber}</td>
+                    <td>${userInfo.like}</td>
+                    <td>${userInfo.dislike}</td>
+                  </tr>`;
+      }
+    }
+    $("#userTable").html(table);
   }
 };
 
 window.App = App;
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   if (window.ethereum) {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
     window.ethereum.enable(); // get permission to access accounts
+    window.ethereum.on("accountsChanged", function () {
+      App.start();
+    });
+    window.ethereum.on("networkChanged", function () {
+      App.start();
+    })
   } else {
     console.warn(
       "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
--- contracts/Weibo.sol
+++ contracts/Weibo.sol
@@ -1,49 +1,53 @@
 pragma solidity >=0.4.21 <0.7.0;
+
 contract Weibo {
     // 管理员
     address admin;
-    uint _numberOfUser;
+    uint256 userNumber;
 
     modifier onlyAdmin() {
         if (msg.sender == admin) _;
     }
-    
+
     // 博文
     struct Blog {
-        uint   timestamp;
+        uint256 timestamp;
         string content;
-        bool   valid;
-        uint   like;
-        uint   dislike;
+        bool isValid;
+        uint256 like;
+        uint256 dislike;
     }
 
     // 微博用户
     struct User {
+        uint256 id;
         address addr;
-        string  name;
-        bool    valid;
-        uint    blogNumber;
-        mapping(uint => Blog) blogs;
+        string name;
+        bool isValid;
+        uint256 blogNumber;
+        mapping(uint256 => Blog) blogs;
     }
 
+    // 存储用户的hash表
     mapping(address => User) users;
     mapping(address => string) address2name;
     mapping(string => address) name2address;
-    mapping(uint => address) id2address;
+    mapping(uint256 => address) id2address;
 
     //构造函数 部署合约的账户为admin账户
     constructor() public {
         admin = msg.sender;
-        _numberOfUser = 0;
+        userNumber = 0;
     }
 
     // 返回admin账户
-    function getAdmin() view public returns (address) {
+    function getAdmin() public view returns (address) {
         return admin;
     }
 
     // 创建用户
     event CreateUser(address sender, bool isSuccess, string message);
+
     function createUser(string memory name) public {
         // name已经被占用
         if (name2address[name] != address(0)) {
@@ -60,20 +64,23 @@ contract Weibo {
             emit CreateUser(msg.sender, false, "昵称长度不合理");
             return;
         }
+        users[msg.sender].id = userNumber;
         users[msg.sender].addr = msg.sender;
         users[msg.sender].name = name;
         users[msg.sender].blogNumber = 0;
-        users[msg.sender].valid = true;
-        id2address[_numberOfUser] = msg.sender;//存储id到用户
-        _numberOfUser++;
+        users[msg.sender].isValid = true;
+        // 保存id到账户的映射关系
+        id2address[userNumber] = msg.sender;
         // 保存账户到昵称的双向映射关系
         address2name[msg.sender] = name;
         name2address[name] = msg.sender;
+        userNumber++;
         emit CreateUser(msg.sender, true, "新用户创建成功");
     }
 
     // 发微博
     event PostBlog(address sender, bool isSuccess, string message);
+
     function postBlog(string memory content) public {
         // 微博长度超过160字节
         if (bytes(content).length == 0 || bytes(content).length > 160) {
@@ -84,98 +91,106 @@ contract Weibo {
         users[msg.sender].blogs[users[msg.sender].blogNumber].content = content;
         users[msg.sender].blogs[users[msg.sender].blogNumber].like = 0;
         users[msg.sender].blogs[users[msg.sender].blogNumber].dislike = 0;
-        users[msg.sender].blogs[users[msg.sender].blogNumber].valid = true;
+        users[msg.sender].blogs[users[msg.sender].blogNumber].isValid = true;
         users[msg.sender].blogNumber++;
         emit PostBlog(msg.sender, true, "微博发布成功");
     }
 
     // 给博文点赞
     event LikeBlog(address sender, bool isSuccess, string message);
-    function likeBlog(address author, uint id) public {
-        if (!users[author].valid) {
+
+    function likeBlog(uint256 user_id, uint256 blog_id) public {
+        address addr = id2address[user_id];
+        if (!users[addr].isValid) {
             emit LikeBlog(msg.sender, false, "用户不存在");
             return;
         }
-        if (!users[author].blogs[id].valid) {
+        if (!users[addr].blogs[blog_id].isValid) {
             emit LikeBlog(msg.sender, false, "博文不存在");
             return;
         }
-        users[author].blogs[id].like += 1;
+        users[addr].blogs[blog_id].like += 1;
         emit LikeBlog(msg.sender, true, "点赞成功");
     }
 
     // 给博文点踩
     event DislikeBlog(address sender, bool isSuccess, string message);
-    function dislikeBlog(address author, uint id) public {
-        if (!users[author].valid) {
+    function dislikeBlog(uint256 user_id, uint256 blog_id) public {
+        address addr = id2address[user_id];
+        if (!users[addr].isValid) {
             emit LikeBlog(msg.sender, false, "用户不存在");
             return;
         }
-        if (!users[author].blogs[id].valid) {
+        if (!users[addr].blogs[blog_id].isValid) {
             emit LikeBlog(msg.sender, false, "博文不存在");
             return;
         }
-        users[author].blogs[id].dislike += 1;
+        users[addr].blogs[blog_id].dislike += 1;
         emit LikeBlog(msg.sender, true, "点踩成功");
     }
 
-    //根据用户的姓名获取地址
-    event GetUserNameByAddr(address sender, bool isSuccess, string name);
-    function getUserNameByAddr(address user) public {
-        if(bytes(address2name[user]).length == 0){
-            emit GetUserNameByAddr(msg.sender, false, "用户不存在");
-            return;
+    // 获取博文总数
+    function getBlogAmount() public view returns (uint256) {
+        uint256 blogAmount = 0;
+        for (uint256 i = 0; i < userNumber; i++) {
+            blogAmount += users[id2address[i]].blogNumber;
         }
-        emit GetUserNameByAddr(msg.sender,true,address2name[user]);
-        return;
+        return blogAmount;
     }
 
-    //根据用户的地址获取姓名
-    event GetUserAddrByName(address sender,bool isSuccess, address user);
-    function getUserAddrByName(string memory name) public {
-        if(name2address[name] == address(0)){
-            emit GetUserNameByAddr(msg.sender,false,"没有该用户");
-        }
-        emit GetUserAddrByName(msg.sender,true,name2address[name]);
+    // 获取用户总数
+    function getUserAmount() public view returns (uint256) {
+        return userNumber;
     }
 
-    //根据id返回是否查询成功、昵称、账户、博文总数、like、dislike
-    function getUserInfo(uint id) view public returns (bool isSuccess,string memory name,address useraddr,uint blogAmount,uint like,uint dislike){
-        if(id2address[id] == address(0)){
-            return(false,"",address(0),0,0,0);
-        }
-        address author = id2address[id];
-        uint all_like = 0;
-        uint all_dislike = 0;
-        string storage u_name = users[author].name;
-        address userAddress = users[author].addr;
-        uint blogNumber = users[author].blogNumber;
-        for(uint i = 0;i < blogNumber ; i++){
-            all_like += users[author].blogs[i].like;
-            all_dislike += users[author].blogs[i].dislike;
-        }
-        return(true,u_name,userAddress,blogNumber,all_like,all_dislike);
-        
+    // 根据根据address获取用户id
+    function getUserId(address addr) public view returns (uint256) {
+        return users[addr].id;
     }
 
-    //整个系统用户总数、博文总数
-    function getUserAmountAndBlogAmount() view public returns(uint userAmount,uint blogAmount){
-        uint Allblogs = 0;
-        for(uint i = 0;i<_numberOfUser;i++){
-            Allblogs += users[id2address[i]].blogNumber;
+    // 根据id返回是否查询成功、昵称、账户、博文数、like、dislike
+    function getUserInfo(uint256 id)
+        public
+        view
+        returns (
+            bool isSuccess,
+            string memory name,
+            address addr,
+            uint256 blogNumber,
+            uint256 like,
+            uint256 dislike
+        )
+    {
+        if (id2address[id] == address(0)) {
+            return (false, "", address(0), 0, 0, 0);
         }
-        return(_numberOfUser,Allblogs);
+        address _addr = id2address[id];
+        uint256 _like = 0;
+        uint256 _dislike = 0;
+        for (uint256 i = 0; i < users[_addr].blogNumber; i++) {
+            _like += users[_addr].blogs[i].like;
+            _dislike += users[_addr].blogs[i].dislike;
+        }
+        return (true, users[_addr].name, users[_addr].addr, users[_addr].blogNumber, _like, _dislike);
     }
 
-    function getBlogByID(uint user_id,uint blog_id) view public returns(bool isSucess,uint timestamp,string memory content,uint like,uint dislike){
-        if(id2address[user_id]==address(0) || users[id2address[user_id]].blogs[blog_id].valid==false){
-            return(false,0,"",0,0);
+    // 根据用户id和博文id返回博文信息
+    function getBlog(uint256 user_id, uint256 blog_id)
+        public
+        view
+        returns (
+            bool isSuccess,
+            uint256 timestamp,
+            string memory content,
+            uint256 like,
+            uint256 dislike
+        )
+    {
+        address addr = id2address[user_id];
+        if (!users[addr].isValid || !users[addr].blogs[blog_id].isValid) {
+            return (false, 0, "", 0, 0);
         }
-        uint blog_timestamp = users[id2address[user_id]].blogs[blog_id].timestamp;
-        string storage blog_content = users[id2address[user_id]].blogs[blog_id].content;
-        uint blog_like = users[id2address[user_id]].blogs[blog_id].like;
-        uint blog_dislike = users[id2address[user_id]].blogs[blog_id].dislike;
-        return(true,blog_timestamp,blog_content,blog_like,blog_dislike);
-
+        Blog memory blog = users[addr].blogs[blog_id];
+        return (true, blog.timestamp, blog.content, blog.like, blog.dislike);
     }
 }
